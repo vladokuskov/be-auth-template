fe0260257e7f3e2874b90a31c1c7113edbb5e6c4
Vladyslav Okuskov <vokuskov@artelogic.net>
2025-01-04 21:30:43 +0200

    Added default err message, added err handling for auth middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -5,6 +5,7 @@ import {NextFunction, Request, Response} from 'express';
 export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
   logger.error(err.stack);
   res.status(500).json({error: appConfig.defaultErrMessage});
+  return;
 };
 
 export default errorMiddleware;
