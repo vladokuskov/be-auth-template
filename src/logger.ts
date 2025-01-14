3c9cb717468005abc5d09c22459e08450bea4a1d
Vladyslav Okuskov <vokuskov@artelogic.net>
2025-01-14 14:54:21 +0200

    Removed pino unused serializer

--- src/logger.ts
+++ src/logger.ts
@@ -8,13 +8,6 @@ const logger = pino({
       ignore: 'pid,hostname', // Ignore pid and hostname in logs
     },
   },
-  serializers: {
-    req: (req) => ({
-      id: req.id,
-      method: req.method,
-      url: req.url,
-    }),
-  },
 });
 
 export {logger};
